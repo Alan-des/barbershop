42663ecf0aa6bb3cd4a51855f66fdb47768a4a41
=Alan-des <alanakhundov2@gmail.com>
2026-03-27 23:01:54 +0200

    up

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,22 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtnHeader: ".header-btn",
+    openModalBtnAbout: ".about-btn",
+    openModalBtn_prices: ".prices-btn",
+    closeModalBtn: "[data-modal-close]",
+    modal: "[data-modal]",
+  };
+
+  function addListeners(selectors, event, handler) {
+    Object.values(selectors).forEach(selector => {
+      if (selector === selectors.modal) return;
+        document.querySelector(selector).addEventListener(event, handler);
+    });
+  }
+
+  function toggleModal() {
+    document.querySelector(refs.modal).classList.toggle("is-hidden");
+  }
+
+  addListeners(refs, "click", toggleModal);
+})();
